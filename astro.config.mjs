1d1ade5338a3ed5a5dffee3f498afbe847b1dd25
Martin Beaumont <hello@martinbeaumont.dev>
2023-09-01 16:59:52 +0200

    Astro 3.0 (#24)
    * upgrade astro 3.0
    
    * upgrade project srcs
    
    * add content example
    
    * Update tsconfig.json
    
    * Update eslint config
    
    * add checks to CI and disable pa11y
    
    * bump node version
    
    * ignore astro internals
    
    * Update package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://site.url',
+  site: 'https://site.url/',
   trailingSlash: 'never',
   build: {
     format: 'file',
